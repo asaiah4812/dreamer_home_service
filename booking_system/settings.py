881c7fc5a3c52b0f3d590d206cbcafcb961fb665
Asaiah Henson <Hensonasaiah21@gmail.com>
2024-05-19 11:07:59 -0700

    intial commit

--- booking_system/settings.py
+++ booking_system/settings.py
@@ -31,10 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if ENVIRONMENT == 'development':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = False
+
 
 
 
